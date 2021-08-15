e730cd120f1009d37ffaee00d4172980553bdb9f
Vendicated <vendicated@riseup.net>
2021-08-15 19:53:37 +0200

    Zip Aliucord.dex

--- build.go
+++ build.go
@@ -28,7 +28,14 @@ func build(project string) {
 		}
 	}
 
-	os.Rename(config.Outputs+"/classes.dex", config.Outputs+"/"+out)
+	if strings.EqualFold(project, "aliucord") {
+		tempDex := config.Outputs + "/classes.dex"
+		os.Rename(f.Name(), tempDex)
+		makeZipWithClasses(config.Outputs, project+".zip", nil)
+		os.Remove(tempDex)
+	} else {
+		os.Rename(f.Name(), config.Outputs + "/Injector.dex")
+	}
 
 	colorPrint(success, "Successfully built "+project)
 }
@@ -96,10 +103,10 @@ func buildPlugin(pluginName string) {
 
 			os.Remove(tmpApk)
 		} else {
-			makeZipWithClasses(out, pluginName)
+			makeZipWithClasses(config.OutputsPlugins, out, &pluginName)
 		}
 	} else {
-		makeZipWithClasses(out, pluginName)
+		makeZipWithClasses(config.OutputsPlugins, out, &pluginName)
 	}
 
 	os.Remove(outputsPlugins + "/classes.dex")
@@ -135,16 +142,18 @@ func zipAndD8(f *os.File, zipw *zip.Writer, javacBuild, zipName, outputPath stri
 	execCmd(os.Stdout, output, "d8", javacBuild+zipName)
 }
 
-func makeZipWithClasses(out, pluginName string) {
-	f, _ := os.Create(config.OutputsPlugins + "/" + out)
+func makeZipWithClasses(outdir, out string, pluginName* string) {
+	f, _ := os.Create(outdir + "/" + out)
 	defer f.Close()
 	zipw := zip.NewWriter(f)
 	defer zipw.Close()
 
-	f, _ = os.Open(config.OutputsPlugins + "/classes.dex")
+	f, _ = os.Open(outdir + "/classes.dex")
 	zipFilew, _ := zipw.Create("classes.dex")
 	io.Copy(zipFilew, f)
 	f.Close()
 
-	writePluginEntry(zipw, pluginName)
+	if pluginName != nil {
+		writePluginEntry(zipw, *pluginName)
+	}
 }
